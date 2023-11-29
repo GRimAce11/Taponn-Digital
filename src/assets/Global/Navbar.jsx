71af973e95845ee8a88b566ed6d0a3b0eb4d38d0
Poojagsb <ppoojabhakta@gmail.com>
2023-11-29 12:32:17 +0530

    chnges

--- src/assets/Global/Navbar.jsx
+++ src/assets/Global/Navbar.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link as Scroll } from 'react-scroll';
+import Close from "../../assets/Navbar/Close1.png";
+import Open from "../../assets/Navbar/Open1.png";
+import Logo from "../../assets/Navbar/sector 9 final.jpg"
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const location = useLocation();
+  const path = location.pathname;
+  const navigate = useNavigate();
+
+  const Navtitle = [
+    { id: "Home", name: "Home", offset: 0 },
+    { id: "Entertainment", name: "Entertainment", offset: -150 },
+    { id: "Food", name: "Food", offset: -48 },
+    { id: "About", name: "About", offset: -100 },
+    { id: "Gallery", name: "Gallery", offset: -100 },
+    { id: "ContactUs", name: "Contact", offset: -47 }
+  ];
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+  const [isSticky, setIsSticky] = useState(false);
+
   return (
-    <div>Navbar</div>
-  )
-}
+   
+      <nav
+        className={`w-full transition-all duration-300 ${isSticky ? 'hidden' : 'fixed top-0 z-[999]'}`}>
+        <div
+          className="navbg w-full h-min bg-[#000000] py-1 ">
+          <div className='w-full m-1  z-20 flex justify-between items-center px-10 text-white'>
+            {path === "/" ?
+              <Scroll to="Home" smooth={true} offset={0} duration={500}>
+                {/* <h1 className="text-2xl text-red-500 font-bold">S9</h1> */}
+                <img src={Logo} width={80} className='navigationbar cursor-pointer' alt="S9" />
+              </Scroll>
+              :
+              <img src={Logo} onClick={() => navigate("/")} width={80} className='navigationbar cursor-pointer' alt="S9" />
+            }
+            <ul className=' hidden lg:flex'>
+              {Navtitle.map((nav,index) => (
+                <li key={index} className="navtitles pt-4 px-4 pb-2 text-center hover:text-red-700  md:hover:bg-transparent">
+                  {path === "/" ?
+                    <Scroll to={nav.id} smooth={true} offset={nav.offset} duration={500} className='Navfont cursor-pointer text-lg'>{nav.name}</Scroll>
+                    :
+                    <Scroll to={nav.id} onClick={() => navigate("/")} smooth={true} offset={nav.offset} duration={500} className='Navfont cursor-pointer text-lg'>{nav.name}</Scroll>
+                  }
+                </li>
+              ))}
+            </ul>
+  
+            {/* Mobile Button */}
+            <div onClick={handleNav} className='block lg:hidden z-[999]'>
+              {nav ? (
+                <img src={Close} width={40} height={50} alt="Close"  />
+              ) : (
+                <img src={Open} width={40} height={50} alt="Ham" />
+              )}
+            </div>
+            {/* Mobile Menu */}
+            <div
+              className={
+                nav
+                  ? 'lg:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in-out duration-300 z-[599]'
+                  : 'lg:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-red-800 text-center ease-in duration-300 '
+              }
+            >
+              <ul >
+                {Navtitle.map((nav,index) => (
+                  <li key={index} onClick={handleNav} className="text-xl text-white py-6 px-6 text-center  hover:text-red-700  hover:rounded-xl border-blue-800 ">
+                    {path === "/" ?
+                    <Scroll to={nav.id} smooth={true} offset={nav.offset} duration={500} onClick={handleNav} className='Navfont cursor-pointer'>{nav.name}</Scroll>
+                    :
+                    <Scroll to={nav.id} onClick={() => navigate("/")} smooth={true} offset={nav.offset} duration={500} className='Navfont cursor-pointer text-lg'>{nav.name}</Scroll>
+                }
+                  </li>
+                ))}
+  
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+  
 
 export default Navbar
