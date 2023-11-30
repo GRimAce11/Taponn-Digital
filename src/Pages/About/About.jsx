09a1e8847ea31153e724352628a2b3b89bd62cb5
Poojagsb <ppoojabhakta@gmail.com>
2023-11-30 12:45:21 +0530

    aboutus color changed

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -6,16 +6,16 @@ const About = () => {
             <div className="container font-primary mx-auto">
                 <div className="flex flex-wrap items-center">
                     <div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
-                        <div className="relative flex flex-col min-w-0 break-words  w-full mb-6 shadow-lg rounded-lg bg-pink-500">
+                        <div className="relative flex flex-col min-w-0 break-words  w-full mb-6 shadow-lg rounded-lg bg-white">
                             <img alt="..." src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" className="w-full align-middle rounded-t-lg" />
                             <blockquote className="relative p-8 mb-4">
                                 <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" className="absolute left-0 w-full block h-[95px] -top-[94px]">
-                                    <polygon points="-30,95 583,95 583,65" class="text-pink-500 fill-current"></polygon>
+                                    <polygon points="-30,95 583,95 583,65" class="text-white fill-current"></polygon>
                                 </svg>
-                                <h4 className="text-xl font-bold text-white">
+                                <h4 className="text-xl font-bold text-black">
                                     Great for your awesome project
                                 </h4>
-                                <p className="text-md font-light mt-2 text-white">
+                                <p className="text-md font-light mt-2 text-black">
                                     Putting together a page has never been easier than matching
                                     together pre-made components. From landing pages presentation
                                     to login areas, you can easily customise and built your pages.
