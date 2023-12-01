7f9141b228bce0d723d31f4e74b62892171a1e0e
GRimAce11 <chethannayak010@gmail.com>
2023-12-01 12:18:23 +0530

    PRicing added to homepage

--- src/Global/Homepage.jsx
+++ src/Global/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from "../Pages/Hero/Hero"
 import About from '../Pages/About/About'
 import Features from '../Pages/Feature/Features'
+import Pricing from '../Pages/Pricing/Pricing'
 import HowToUse from '../Pages/HowToUse/HowToUse'
 const Homepage = () => {
     return (
